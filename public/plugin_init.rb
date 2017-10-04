1e77b12cd8469e39ee5ecd74d7af70b14979f8df
Bobbi Fox <bobbi_fox@harvard.edu>
2017-10-04 17:20:19 -0400

    calling the stylesheet

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -1,5 +1,5 @@
 # sets up the AppConfig to conform to Harvard's needs
 AppConfig[:pui_hide][:accessions] = true
 AppConfig[:pui_hide][:classifications] = true
-AppConfig[:pui_branding_img] = '/assets/hl_logo.png'
+AppConfig[:pui_branding_img] = '/assets/hl_logo_alt.png'
 # AppConfig[:pui_branding_img] = 'Aspace-logo-transparent.png'
