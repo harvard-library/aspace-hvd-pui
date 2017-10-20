70fb029945e88aad45d8c9d272bfc61c86a26eb8
Bobbi Fox <bobbi_fox@harvard.edu>
2017-10-20 13:01:35 -0400

    replace 'To' with '--' in search filtering

--- public/assets/harvard.js
+++ public/assets/harvard.js
@@ -1,4 +1,14 @@
 $(function() {
-/*    alert("hi there!"); */
+/* this really should be taken care of by a pull request to tack '-label' on to form labels, but... */
+
+/* replace the "To" label with "-- " */
+
+   var $inlines = $(".search form .inline-label");
+   $inlines.each(function(){
+       var $lbl = $(this); 
+       if ($lbl.text() == "To")
+       {$lbl.text("--")}
+   });
+
 });
 
