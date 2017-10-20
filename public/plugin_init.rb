5d221087035881a7dc5494da1612d83fc74cad92
Bobbi Fox <bobbi_fox@harvard.edu>
2017-10-20 13:13:08 -0400

    support email (but see README)

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -4,3 +4,18 @@ AppConfig[:pui_hide][:classifications] = true
 AppConfig[:pui_branding_img] = '/assets/hl_logo_alt.png'
 AppConfig[:pui_hide][:classification_badge] = true
 # AppConfig[:pui_branding_img] = 'Aspace-logo-transparent.png'
+AppConfig[:pui_page_custom_actions] << {
+   'record_type' => ['resource'], # the jsonmodel type to show for
+   'erb_partial' => 'shared/test'
+   }
+#AppConfig[:pui_email_enabled] = true
+
+AppConfig[:pui_request_email_fallback_to_address] = 'bobbi_fox@harvard.edu'
+AppConfig[:pui_request_email_fallback_from_address] = 'bobbi_fox@harvard.edu'
+#AppConfig[:pui_email_delivery_method] = :sendmail
+#AppConfig[:pui_email_sendmail_settings] = {
+#  location: '/usr/sbin/sendmail',
+#  arguments: '-i'
+# }
+
+
