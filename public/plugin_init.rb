7667a2fbbe22b6c9b28447e6475ba83fbf106a60
Bobbi Fox <bobbi_fox@harvard.edu>
2017-10-30 16:21:13 -0400

    support Creator facetting

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -18,4 +18,24 @@ AppConfig[:pui_request_email_fallback_from_address] = 'bobbi_fox@harvard.edu'
 #  arguments: '-i'
 # }
 
+## override the resources#index facetting
+Rails.application.config.after_initialize do
+
+  Searchable.module_eval do
+    def set_up_and_run_search(default_types = [],default_facets=[],default_search_opts={}, params={})
+      if default_types.length == 1 && default_types[0] == 'resource'
+        default_facets =  %w{primary_type creators subjects published_agents }
+        Pry::ColorPrinter.pp ["FACETS:", default_facets]
+      end
+      set_up_advanced_search(default_types, default_facets, default_search_opts, params)
+      page = Integer(params.fetch(:page, "1"))
+      @results =  archivesspace.advanced_search('/search', page, @criteria)
+      if @results['total_hits'].blank? ||  @results['total_hits'] == 0
+        raise NoResultsError.new
+      else
+        process_search_results(@base_search)
+      end
+    end
+  end
+end 
 
