dc1d49f9a1926a4529ec61bbd67ae1399ef64d81
Bobbi Fox <bobbi_fox@harvard.edu>
2018-01-10 10:58:55 -0500

    Remove bad digital routes

--- public/routes.rb
+++ public/routes.rb
@@ -1,5 +1,6 @@
 ArchivesSpacePublic::Application.routes.draw do
   match 'repositories/:rid/resources/:id/digct' => 'resources_addons#digital_object_count', :via => [:get]
   match 'repositories/:rid/resources/:id/digital_only' => 'resources#digital_only', :via => [:get, :post]
-
+  match 'repositories/:rid/resources/:id/request' => 'resources_addons#request_popup', :via => [:get, :post]
+  match 'repositories/:rid/archival_objects/:id/request' => 'resources_addons#request_popup', :via => [:get, :post]
 end
