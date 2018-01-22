3df34356f4bad725180d9237ab4b0bf89fa4f034
Bobbi Fox <bobbi_fox@harvard.edu>
2018-01-22 12:59:51 -0500

    Add Ask a Librarian/Archivist

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -6,7 +6,7 @@ AppConfig[:pui_hide][:classification_badge] = true
 AppConfig[:pui_hide][:container_inventory]=true
 AppConfig[:pui_page_custom_actions] << {
    'record_type' => ['resource'], # the jsonmodel type to show for
-   'erb_partial' => 'shared/test'
+   'erb_partial' => 'shared/ask'
    }
 #AppConfig[:pui_email_enabled] = true
 
