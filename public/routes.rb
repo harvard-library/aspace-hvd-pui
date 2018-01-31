1b27c94897f84dd40ed925473f8d661a3b5134c8
Bobbi Fox <bobbi_fox@harvard.edu>
2018-01-31 16:19:17 -0500

    beginnings of PDF modifications

--- public/routes.rb
+++ public/routes.rb
@@ -4,4 +4,7 @@ ArchivesSpacePublic::Application.routes.draw do
   match 'repositories/:rid/resources/:id/request' => 'resources_addons#request_popup', :via => [:get, :post]
   match 'repositories/:rid/archival_objects/:id/request' => 'resources_addons#request_popup', :via => [:get, :post]
   match 'repositories/:rid/resources/:id/csv' => 'resources_addons#csv_out', :via => [:get], defaults: { format: 'csv' }
+  match 'repositories/:rid/resources/:id/hvdpdf' => 'hvd_pdf#resource', :via => [:get], defaults: { format: 'pdf' }
+  match 'repositories/:rid/resources/:id/exp' => 'resources_addons#experiment', :via => [:get, :post]
+  
 end
