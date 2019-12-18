41fe0184457382e23716316342c75ee9c53e752c
Douglas Simon <douglas_simon@harvard.edu>
2019-12-18 09:19:17 -0500

    partially done, working on resolving escape to close dropdown issue

---
+++ public/assets/tabbable_dropdowns.js
@@ -0,0 +1,20 @@
+const toggleDropdown = (event) => {
+    // if (event.keyCode === 13) {
+    //     event.preventDefault();
+    //     const currentElement = document.getElementById("dropdownMenu1");
+    //     console.log(currentElement.id)
+    //     $("#dropdownMenu1").dropdown("toggle");
+    // } else if (event.keyCode === 27) {
+    //     event.preventDefault();
+    //     $('.open').removeClass('open');
+    //     $("#dropdownMenu1").focus();
+    // }
+}
+
+const addDropdownToggle = (element) => {
+    // document.addEventListener("keydown", toggleDropdown, false);
+}
+
+const removeDropdownToggle = (element) => {
+    // document.removeEventListener("keydown", toggleDropdown, false)
+}
