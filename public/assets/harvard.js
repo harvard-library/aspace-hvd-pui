42da83b5f4569cfcc8f25ff3d2a10102921b2b5f
Hurlyburly2 <douglas_simon@harvard.edu>
2020-03-05 11:56:16 -0500

    added focus to dropdown arrow in sidebar, ask a librarian guide opens in new tab, fixed sidebar search field orientation back to centered

--- public/assets/harvard.js
+++ public/assets/harvard.js
@@ -265,4 +265,12 @@ $(window).on('load', function() {
   $("body").on('keydown', '.more.btn.refine, .less.btn.refine', function(event) {
     toggleMoreFacets(event)
   })
+
+  $("button.expandme").focusin(function() {
+    $(this).find("i").addClass("focusIcon")
+  })
+
+  $("button.expandme").focusout(function() {
+    $(this).find("i").removeClass("focusIcon")
+  })
 })
