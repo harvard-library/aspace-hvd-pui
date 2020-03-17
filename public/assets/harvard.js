4e18d1ac291eba8bdce1003a884828d27e4b3f93
Hurlyburly2 <douglas_simon@harvard.edu>
2020-03-17 10:35:37 -0400

    bug fix

--- public/assets/harvard.js
+++ public/assets/harvard.js
@@ -282,6 +282,7 @@ $(window).on('load', function() {
 
   $("button.expandme").focusout(function() {
     $(this).find("i").removeClass("focusIcon")
+  })
 
   $("body").on('change', 'select.search-keyword, select.limit-field', function(event) {
     handleDropdownCarets(event)
