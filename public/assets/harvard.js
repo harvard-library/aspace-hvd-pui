b283fb37222beced54a96226e599eb0858e4a498
vac765 <valdeva_crema@harvard.edu>
2020-05-06 20:54:59 -0400

    Handling the scrolling page of records that can be requested.

--- public/assets/harvard.js
+++ public/assets/harvard.js
@@ -280,17 +280,34 @@ $(window).on('load', function() {
     handleDropdownCarets(event)
   })
 
+  //Make sure the request number is updated on page load
   updateRequestNumber();
   
-  //If the RL Action Button is present, add a listener there.
-  if ($(".request_list_action_button") != null) {
+  //When this scrolls, first unbind the click function for updating the
+  //request number on all buttons and then add it back.  This makes sure
+  //that any buttons that have it don't get multiple instances of the handler
+  //added and that any buttons that don't yet have it have it added.
+  $(".infinite-record-scrollbar").on('scroll', function(){
+	  $(".request_list_action_button").unbind('click', updateRequestNumber);
 	  $(".request_list_action_button").on('click', updateRequestNumber);
+  });
+
+  //If one or more action buttons have loaded, add the click listener and 
+  //update handler.  After this, as the user scrolls (if this is a scrolling page), 
+  //the handlers will be added to the click event.
+  if ($(".request_list_action_button").length > 0) {
+      $(".request_list_action_button").on('click', updateRequestNumber);
+  }
+  //otherwise wait half a second for the partial to finish loading and then add it.
+  else {
+      setTimeout(function(){ $(".request_list_action_button").on('click', updateRequestNumber) }, 500);
   }
 });
 
+//Get the request_list object if it exists.  **The request_list object is from the request_list plugin.
+//The request_list object will have the number of items currently in the request_list.
+//Set the number in the top menu REQUEST (#) value.
 function updateRequestNumber() {
-	// Copy the number of requests from the menu option to the top "REQUEST" level. Intended behavior
-	// is leaving it in both places, so no need to remove
 	if (window.request_list != null) {
 		var requestNumber = window.request_list.getList().length;
 		$("#request_list_top_menu").find("span").text($("#request_list_top_menu").find("span").text().replace(/ \(\d+\)/, " (" + requestNumber + ")"));
