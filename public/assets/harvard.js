ebbc76409332750ab508547ae96b7938fc6467c5
vac765 <valdeva_crema@harvard.edu>
2020-05-06 21:11:50 -0400

    Added in a listener and handler for the page that shows the request lists and any delete buttons that are used on that page.

--- public/assets/harvard.js
+++ public/assets/harvard.js
@@ -302,6 +302,13 @@ $(window).on('load', function() {
   else {
       setTimeout(function(){ $(".request_list_action_button").on('click', updateRequestNumber) }, 500);
   }
+  
+  //If this is the page that is displaying the list of requests, the user can
+  //remove requests from that list.  Add a listener and update request number handler
+  //to those buttons as well.
+  if ($(".rl-remove-from-list-button").length > 0) {
+	  $(".rl-remove-from-list-button").on('click', updateRequestNumber);
+  }
 });
 
 //Get the request_list object if it exists.  **The request_list object is from the request_list plugin.
