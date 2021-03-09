c8cab2cead647662530b22c91bb5c350f7243fe8
Hurlyburly2 <douglas_simon@harvard.edu>
2021-03-09 16:27:34 -0500

    Fixed deprecated skip_before_filter method

--- public/controllers/resources_addons_controller.rb
+++ public/controllers/resources_addons_controller.rb
@@ -7,7 +7,7 @@ class ResourcesAddonsController < ApplicationController
   helper_method :process_subjects
   helper_method :process_agents
 
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
  # produce a CSV
  def csv_out
