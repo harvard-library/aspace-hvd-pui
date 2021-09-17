4c5e653b8539e708ec8cfa9bb9a2735c031cb215
Hurlyburly2 <douglas_simon@harvard.edu>
2021-09-17 17:08:44 -0400

    overrode cite_item_description method to use component_id instead of identifier

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -159,6 +159,32 @@ Rails.application.config.after_initialize do
       cite += " #{ cite_url_and_timestamp}"
       cite
     end
+
+    # Method overridden to use component_id instead of identifier
+    def cite_item_description
+      cite = note('prefercite')
+      if !cite.blank?
+        cite = strip_mixed_content(cite['note_text'])
+      else
+        cite = strip_mixed_content(display_string)
+        cite += json.key?('component_id') ? ", #{json['component_id']}" : ''
+        cite += if container_display.blank? || container_display.length > 5
+                  '.'
+                else
+                  @citation_container_display ||= parse_container_display(:citation => true).join('; ')
+                  ", #{@citation_container_display}."
+                end
+        if resolved_resource
+          ttl = resolved_resource.dig('title')
+          cite += " #{strip_mixed_content(ttl)}, #{resource_identifier}."
+        end
+        unless repository_information['top']['name'].blank?
+          cite += " #{ repository_information['top']['name']}."
+        end
+      end
+      HTMLEntities.new.decode("#{cite}   #{cite_url_and_timestamp}.")
+    end
+
   end
 
 
